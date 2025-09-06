32c32afde6581ea90c01e3736ea6051c3e0fc2e4
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-07 06:19:37 +0800

    Sun Sep  7 06:19:37 AM CST 2025

--- .make/depend.dir/libcpp.mk
+++ .make/depend.dir/libcpp.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libcpp.recursive
 ECHO_TARGETS += libcpp.echo
 libcpp.recursive:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libcpp/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libcpp/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libcpp/ && make recursive && make install
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libvk/               >> /home/lidali/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libvk/.make >> /home/lidali/alpine/libraries/libcpp/.make/super
+	cd /home/lidali/alpine/libraries/libcpp/ && make recursive && make install
+	-rm /home/lidali/alpine/libraries/libcpp/.make/super
 libcpp.echo:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libcpp/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libcpp/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libcpp/ && make echo
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libvk/               >> /home/lidali/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libvk/.make >> /home/lidali/alpine/libraries/libcpp/.make/super
+	cd /home/lidali/alpine/libraries/libcpp/ && make echo
+	-rm /home/lidali/alpine/libraries/libcpp/.make/super
