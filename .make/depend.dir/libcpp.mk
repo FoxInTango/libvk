aa3b42f9a6cb4197da98cb2be8dec85439e527e6
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:55 +0800

    2025年10月14日 星期二 15时57分54秒 CST

--- .make/depend.dir/libcpp.mk
+++ .make/depend.dir/libcpp.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libcpp.recursive
 ECHO_TARGETS += libcpp.echo
 libcpp.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libvk/               >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libvk/.make >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
-	cd /volumes/llama/home/alpine/libraries/libcpp/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libvk/               >> /Users/lidali/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libvk/.make >> /Users/lidali/alpine/libraries/libcpp/.make/super
+	cd /Users/lidali/alpine/libraries/libcpp/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libcpp/.make/super
 libcpp.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libvk/               >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libvk/.make >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
-	cd /volumes/llama/home/alpine/libraries/libcpp/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libvk/               >> /Users/lidali/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libvk/.make >> /Users/lidali/alpine/libraries/libcpp/.make/super
+	cd /Users/lidali/alpine/libraries/libcpp/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libcpp/.make/super
